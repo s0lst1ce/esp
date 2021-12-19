9e152204970eef4ca262b61f4e5dd7e0e04fd1a3
s0lst1ce <thithib.cohergne@gmail.com>
2021-12-19 20:30:59 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import random
+
+
+def get_measures(amount: int, mean, sigma):
+    return [random.gauss(mean, sigma) for _ in range(amount)]
+
+
+def main():
+    print("all is well")
+
+
+# build an example dict for the esp8266
+
+
+def expenctancy(args):
+    """this is only sort of an expenctancy.
+    Indeed, although it enventually performs the correct calculation
+    it makes use of the specific formula used in this scenario to optimize away the very notion of probability.
+    This whole simplification process brings it down to a simple weighted average."""
+    pass
+
+
+def variance(probabilities):
+    pass
+
+
+def sqrt(value):
+    pass
+
+
+def path_loss_params_estimation(s1, s2):
+    pass
+
+
+if __name__ == "__main__":
+    main()
