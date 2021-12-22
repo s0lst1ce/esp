cdb6201570b45b03683886a38ea6b79535daf927
s0lst1ce <thithib.cohergne@gmail.com>
2021-12-22 12:47:08 +0100

    typo

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def plot_power(signals, real_mean, real_sigma):
 
 def main():
     """
-    Effectue successivement les différentes visualisation requises pour l'étude.
+    Effectue successivement les différentes visualisations requises pour l'étude.
     """
     print("Running ESP8266 positionning simulation")
     # plot_power(get_measures(4096, -55, 1.6))
